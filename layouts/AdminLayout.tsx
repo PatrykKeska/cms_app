995e4cd4b9d0bbf337a0d7f3f2d07a08cf50b134
PatrykKeska <PatrykKeska4@gmail.com>
2022-12-14 10:00:56 +0100

    Admin panel is ready to marge.

---
+++ layouts/AdminLayout.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+import { FC, ReactNode } from 'react'
+import AdminNav from './AdminNav'
+import { AiOutlineFileAdd } from 'react-icons/ai'
+
+interface Props {
+    children: ReactNode
+}
+
+const AdminLayout: FC<Props> = ({ children }): JSX.Element => {
+    return (
+        <section className='flex flex-row'>
+            <AdminNav />
+            <div className='flex flex-1 p-5'>{children}</div>
+            <Link
+                className='fixed right-10 bottom-10 z-10 rounded-full bg-secondary-dark p-2 text-primary shadow-sm transition hover:scale-90 dark:bg-secondary-light dark:text-primary-dark'
+                href='admin/create/post'
+            >
+                <AiOutlineFileAdd size={25} />
+            </Link>
+        </section>
+    )
+}
+
+export default AdminLayout
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,13 +1,28 @@
 import { NextPage } from 'next'
+import AdminLayout from '../../layouts/AdminLayout'
 import AdminNav from '../../layouts/AdminNav'
 
 interface Props {}
 
 const Admin: NextPage<Props> = () => {
     return (
-        <section>
-            <AdminNav />
-        </section>
+        <AdminLayout>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
+                quis animi, laborum qui error deserunt, ratione modi eum
+                voluptates atque, sint ipsa dolores earum inventore
+                necessitatibus tenetur illo? Ipsum in, quo repudiandae maiores
+                ad distinctio adipisci sed error, dignissimos, accusantium atque
+                voluptatibus! Eveniet pariatur, voluptate voluptates sequi
+                labore laboriosam tenetur tempore ad vero eos cumque alias,
+                doloribus exercitationem assumenda amet ex blanditiis! Harum
+                numquam tempore magni ea explicabo exercitationem consequuntur.
+                Incidunt dolor error a debitis quo dolore, at voluptatum
+                officiis placeat temporibus, quas quisquam eligendi explicabo
+                voluptates deleniti ab itaque iste rerum velit? Aliquid natus
+                quis, perspiciatis saepe nostrum debitis.
+            </p>
+        </AdminLayout>
     )
 }
 
