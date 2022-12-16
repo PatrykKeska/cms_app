cd8c7fd02a594302eb47759d13454f4dd1c47dcd
PatrykKeska <PatrykKeska4@gmail.com>
2022-12-16 14:40:52 +0100

    added tailwindcss typography plugin to handle styling in our text editor.

--- components/common/text_editor/Tiptap.tsx
+++ components/common/text_editor/Tiptap.tsx
@@ -6,12 +6,17 @@ import Toolbar from './Toolbar/Toolbar'
 const Tiptap = () => {
     const editor = useEditor({
         extensions: [StarterKit, Underline],
-        content: '<p>Hello World!</p>',
+        editorProps: {
+            attributes: {
+                class: 'h-full max-w-full mx-auto  prose prose-lg focus:outline-none dark:prose-invert',
+            },
+        },
     })
 
     return (
         <div className='mx-auto flex max-w-4xl flex-col'>
             <Toolbar editor={editor} />
+            <div className='h-[1px] w-full bg-secondary-dark dark:bg-secondary-light' />
             <EditorContent editor={editor} />
         </div>
     )
