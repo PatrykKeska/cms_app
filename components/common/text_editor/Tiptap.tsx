477320769eeb90304c8030bc261d396fb8e0f6f7
PatrykKeska <PatrykKeska4@gmail.com>
2023-02-14 19:00:30 +0100

    Added extensions for tiptap, including all necessary configurations

--- components/common/text_editor/Tiptap.tsx
+++ components/common/text_editor/Tiptap.tsx
@@ -1,22 +1,55 @@
+import BubbleMenu from '@tiptap/extension-bubble-menu'
+import Link from '@tiptap/extension-link'
+import Placeholder from '@tiptap/extension-placeholder'
 import Underline from '@tiptap/extension-underline'
-import { useEditor, EditorContent } from '@tiptap/react'
+import { useEditor, EditorContent, getMarkRange, Range } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import { useEffect, useState } from 'react'
+import EditLink from './Link/EditLink'
 import Toolbar from './Toolbar/Toolbar'
 
 const Tiptap = () => {
+    const [selectionRange, setSelectionRange] = useState<Range>()
     const editor = useEditor({
-        extensions: [StarterKit, Underline],
+        extensions: [
+            StarterKit,
+            Underline,
+            Link.configure({
+                autolink: false,
+                linkOnPaste: false,
+                openOnClick: false,
+                HTMLAttributes: { target: '' },
+                protocols: ['ftp', 'mailto'],
+            }),
+            Placeholder.configure({
+                placeholder: "Let's create something amazing!",
+            }),
+        ],
         editorProps: {
+            handleClick(view, position, event) {
+                const { state } = view
+                const range = getMarkRange(
+                    state.doc.resolve(position),
+                    state.schema.marks.link
+                )
+                if (range) setSelectionRange(range)
+            },
             attributes: {
                 class: 'h-full max-w-full mx-auto  prose prose-lg focus:outline-none dark:prose-invert',
             },
         },
     })
+    useEffect(() => {
+        if (editor && selectionRange) {
+            editor.commands.setTextSelection(selectionRange)
+        }
+    }, [editor, selectionRange])
 
     return (
         <div className='mx-auto flex max-w-4xl flex-col'>
             <Toolbar editor={editor} />
-            <div className='h-[1px] w-full bg-secondary-dark dark:bg-secondary-light' />
+            <div className='mb-2 h-[1px] w-full bg-secondary-dark dark:bg-secondary-light' />
+            {editor ? <EditLink editor={editor} /> : null}
             <EditorContent editor={editor} />
         </div>
     )
