412223ebf93fdf766689962b741cd977b7bf7c71
PatrykKeska <PatrykKeska4@gmail.com>
2023-02-14 19:02:44 +0100

    added a new buttons for toolbar, and logic

--- layouts/Toolbar/ToolbarButtons.tsx
+++ layouts/Toolbar/ToolbarButtons.tsx
@@ -16,28 +16,48 @@ import {
 import { RiDoubleQuotesL } from 'react-icons/ri'
 import { Editor } from '@tiptap/core'
 import { getFocusedEditor } from '../../components/common/text_editor/EditorUtils'
+import InsertLink from '../../components/common/text_editor/Link/InsertLink'
+import { LinkOption } from '../../components/common/text_editor/Link/LinkForm'
 interface Props {
     editor: Editor
 }
 
 const ToolbarButtons: FC<Props> = ({ editor }): JSX.Element => {
+    const handleLinkSubmit = ({ url, openInNewTab }: LinkOption) => {
+        const { commands } = editor
+        if (openInNewTab) {
+            commands.setLink({ href: `${url}`, target: '_blank' })
+        } else {
+            commands.setLink({ href: `${url}` })
+        }
+    }
+
     return (
         <>
-            <Button onClick={() => getFocusedEditor(editor).toggleBold().run()}>
+            <div className='h- mx-3 h-6 w-[1px] border-r-2 bg-secondary-dark dark:bg-secondary-light' />
+            <Button
+                active={editor.isActive('bold')}
+                onClick={() => getFocusedEditor(editor).toggleBold().run()}
+            >
                 <BsTypeBold />
             </Button>
 
             <Button
+                active={editor.isActive('italic')}
                 onClick={() => getFocusedEditor(editor).toggleItalic().run()}
             >
                 <BsTypeItalic />
             </Button>
 
-            <Button onClick={() => editor.commands.toggleUnderline()}>
+            <Button
+                active={editor.isActive('underline')}
+                onClick={() => editor.commands.toggleUnderline()}
+            >
                 <BsTypeUnderline />
             </Button>
 
             <Button
+                active={editor.isActive('strike')}
                 onClick={() => getFocusedEditor(editor).toggleStrike().run()}
             >
                 <BsTypeStrikethrough />
@@ -45,6 +65,7 @@ const ToolbarButtons: FC<Props> = ({ editor }): JSX.Element => {
 
             <div className='h- mx-3 h-6 w-[1px] border-r-2 bg-secondary-dark dark:bg-secondary-light' />
             <Button
+                active={editor.isActive('blockquote')}
                 onClick={() =>
                     getFocusedEditor(editor).toggleBlockquote().run()
                 }
@@ -52,21 +73,24 @@ const ToolbarButtons: FC<Props> = ({ editor }): JSX.Element => {
                 <RiDoubleQuotesL />
             </Button>
 
-            <Button onClick={() => getFocusedEditor(editor).toggleCode().run()}>
+            <Button
+                active={editor.isActive('code')}
+                onClick={() => getFocusedEditor(editor).toggleCode().run()}
+            >
                 <BsCode />
             </Button>
 
             <Button
+                active={editor.isActive('codeBlock')}
                 onClick={() => getFocusedEditor(editor).toggleCodeBlock().run()}
             >
                 <BsBraces />
             </Button>
 
-            <Button>
-                <BsLink45Deg />
-            </Button>
+            <InsertLink onSubmit={handleLinkSubmit} />
 
             <Button
+                active={editor.isActive('orderedList')}
                 onClick={() =>
                     getFocusedEditor(editor).toggleOrderedList().run()
                 }
@@ -75,6 +99,7 @@ const ToolbarButtons: FC<Props> = ({ editor }): JSX.Element => {
             </Button>
 
             <Button
+                active={editor.isActive('bulletList')}
                 onClick={() =>
                     getFocusedEditor(editor).toggleBulletList().run()
                 }
